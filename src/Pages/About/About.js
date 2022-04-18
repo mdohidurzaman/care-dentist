e71aeced992f9454d85acb4b362f96bd825efce0
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-04-18 21:07:58 +0600

    Added about page content

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,27 @@
 import React from "react";
+import img from "../../images/icon/img.jpg";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>This About page.</h1>
+    <div className="about">
+      <div className="profile-img">
+        <img src={img} alt="" />
+      </div>
+      <div className="about-me">
+        <h3>
+          <strong>About Me</strong>
+        </h3>
+        <p>
+          I am Wahidurzaman and I have completed merter in Economics from
+          National University. I am working a germents factory as a
+          merchandiser. I want to change the present job if I can complete the
+          course successfully. I always feel comfortable to work in IT sector
+          and I am hardly trying to complete the course as your instructions.
+          Hopfully I will reach to my dream goal if I get your support as
+          present.
+        </p>
+      </div>
     </div>
   );
 };
